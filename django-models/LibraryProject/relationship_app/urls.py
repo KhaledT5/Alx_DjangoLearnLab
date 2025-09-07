899905555e2cf4157dcbb90eb6e562846fcbc0f1
KhaledT5 <natorukh12345@gmail.com>
2025-09-07 23:59:52 +0300

    fix attempt end

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # exact string for FBV
-from .views import login_view, logout_view, register_view
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns += [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('register/', views.register, name='register'),  # FBV
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
 
