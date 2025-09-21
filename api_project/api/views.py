5ba26bb05846e0987ced762046fdf947d81c54c0
KhaledT5 <natorukh12345@gmail.com>
2025-09-21 22:29:56 +0300

    try to fix

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,4 @@
-from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
-from .models import Book
-from .serializers import BookSerializer
+from django.shortcuts import HttpResponse
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+def placeholder(request):
+    return HttpResponse("API app placeholder")
