c78127bcf6d9c64c76e3179c84c2eb6a9206fb5d
KhaledT5 <natorukh12345@gmail.com>
2025-09-21 22:56:44 +0300

    task 2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from api.models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Provides list, create, retrieve, update, partial_update and destroy actions for Book.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
